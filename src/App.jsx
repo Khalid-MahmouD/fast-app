f6329d392783815094ad218507ece9c60ef37e34
Khalid-MahmouD <khaloooood827@gmail.com>
2025-04-02 20:51:53 +0200

    react router new syntax

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Home from "./ui/Home"
+import Menu from './features/menu/Menu'
+import Cart from './features/cart/Cart'
+import Order from './features/order/Order'
+import CreateOrder from './features/order/CreateOrder'
+
+// handle page nor 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  { path: '/menu', element: <Menu /> },
+  {
+    path: '/cart', element: <Cart />
+  },
+  {
+    path: '/order/new', element: <CreateOrder />,
+  },
+  { path: '/order/:orderId', element: <Order />, },
+]
+)
 function App() {
   // const x = 23;
 
   return (
-    <div>
-      HELLO THERE!!
-    </div>
+    <RouterProvider router={router} />
   )
 }
 
