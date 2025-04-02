28fba605bcbf9b6bcd854869d2ca9a34c3205894
Khalid-MahmouD <khaloooood827@gmail.com>
2025-04-02 23:44:07 +0200

    navigation state and loader

--- src/UI/AppLayout.jsx
+++ src/UI/AppLayout.jsx
@@ -1,16 +1,26 @@
 import CartOverview from "../features/cart/CartOverview"
 import Header from "./Header"
-import { Outlet } from "react-router-dom"
-
+import { Outlet, useNavigation } from "react-router-dom"
+import Loader from "./Loader";
 function AppLayout() {
+    const navigation = useNavigation();
+    const isLoading = navigation.state === "loading";
+
     return (
-        <div>
+        <div className="layout">
+
+            {
+                isLoading && <Loader />
+            }
+
             <Header />
             <main>
+
                 <Outlet />
+
             </main>
             <CartOverview />
-        </div>
+        </div >
     )
 }
 
