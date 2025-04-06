bbb3040f81dfaba45fc8bd05bdc4221c3a1cdb37
Khalid-MahmouD <khaloooood827@gmail.com>
2025-04-06 15:26:01 +0200

    user state with redux

---
+++ src/features/user/Username.jsx
@@ -0,0 +1,13 @@
+import { useSelector } from "react-redux"
+
+function Username() {
+    const username = useSelector((state) => state.user.username)
+    if (!username) return null
+    return (
+        <div>
+            {username}
+        </div>
+    )
+}
+
+export default Username
