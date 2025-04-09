a5970b5a48a42f4072d51b3a292a3ce1f050fe2b
Khalid-MahmouD <khaloooood827@gmail.com>
2025-04-09 14:06:42 +0200

    styling the page

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,11 +1,11 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 function CartOverview() {
   return (
-    <div>
-      <p>
+    <div className="flex items-center justify-between bg-stone-800 p-4 uppercase text-stone-200 sm:p-6 md:text-base">
+      <p className="space-x-4 font-semibold text-stone-300 sm:space-x-6">
         <span>23 pizzas</span>
-        <span>$23.45</span>
+        <span>$23.45</span>x
       </p>
       <Link to="/cart"> Open cart &rarr; </Link>
       {/* <a href="#">Open cart &rarr;</a> */}
--- src/features/user/CreateUser.jsx
+++ src/features/user/CreateUser.jsx
@@ -1,18 +1,30 @@
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { updateName } from './userSlice';
+import { useNavigate, useNavigation } from 'react-router-dom';
 
 function CreateUser() {
   const [username, setUsername] = useState('');
-
+  const navigation = useNavigation();
+  const navigate = useNavigate();
+  const isSubmitting = navigation.state === 'submitting';
+  const dispatch = useDispatch();
   function handleSubmit(e) {
     e.preventDefault();
+    if (!username) return;
 
+    dispatch(updateName(username));
+    navigate('/menu');
   }
 
   return (
     <form onSubmit={handleSubmit}>
-      <p>👋 Welcome! Please start by telling us your name:</p>
+      <p className="mb-4 text-sm text-yellow-600 md:text-base">
+        👋 Welcome! Please start by telling us your name:
+      </p>
 
       <input
+        className="w-72"
         type="text"
         placeholder="Your full name"
         value={username}
@@ -21,7 +33,9 @@ function CreateUser() {
 
       {username !== '' && (
         <div>
-          <button>Start ordering</button>
+          <button disabled={isSubmitting}>
+            {isSubmitting ? 'submitting...' : 'Order'}
+          </button>
         </div>
       )}
     </form>
